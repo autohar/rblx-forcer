ab8872953c697ffabc82ed1dd86eccf2c223daf1
autohar <autoharv2@gmail.com>
2025-10-20 12:49:07 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2563EB", // nice blue accent
+      },
+    },
   },
   plugins: [],
 };
